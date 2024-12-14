7ebf8290a6d7ade6fa4ff32ba6f426189b1133b5
Ruan Sampaio <ruansampaio.works@gmail.com>
2024-12-14 16:32:52 -0300

    feat: "setting up hero section"

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,27 @@
+import { AppBar, MenuItem, styled, Toolbar } from "@mui/material"
+
+
+
+const NavBar = () => {
+  
+    const StyledToobar = styled(Toolbar)(()=> ({
+display: 'flex',
+justifyContent: 'space-evenly',
+
+
+    }))
+
+    return (
+      <>
+        <AppBar position="relative">
+            <StyledToobar>
+            <MenuItem>Sobre</MenuItem>
+            <MenuItem>Habilidades</MenuItem>
+            <MenuItem>Projetos</MenuItem>
+            </StyledToobar>
+        </AppBar>
+      </>
+    )
+  }
+  
+  export default NavBar
