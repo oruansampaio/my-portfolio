60262a1d364a888678984a0742fa174b508c219b
Ruan Sampaio <ruansampaio.works@gmail.com>
2024-12-17 16:00:58 -0300

    sss

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-portfolio/',  // Substitua "my-portfolio" pelo nome do seu repositório
 })
+
